7bf0db004dd1c1cb6e3ab3605dca0d4ed0f04698
jbhagat <1@1.com>
2017-01-21 01:58:57 -0500

    s

---
+++ app/src/main/java/com/vaultapp/pennapps/vaultapp/Communications.java
@@ -0,0 +1,14 @@
+package com.vaultapp.pennapps.vaultapp;
+
+/**
+ * Created by hhbhagat on 1/21/2017.
+ */
+
+public class Communications {
+    public static void sendButtonCall(){
+
+    }
+    public static void sendNessieCall(){
+
+    }
+}
