b38894f74b5d49b0ee77f35a1cd60ae86e11990d
Huemanh <huynh.hueanh1@gmail.com>
2017-01-22 06:45:48 -0500

    layout

---
+++ app/src/main/java/com/vaultapp/pennapps/vaultapp/CustomerCreate.java
@@ -0,0 +1,13 @@
+package com.vaultapp.pennapps.vaultapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomerCreate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_create);
+    }
+}
