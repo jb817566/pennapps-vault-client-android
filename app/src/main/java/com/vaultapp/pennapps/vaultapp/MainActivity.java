257fa873ec7583a4f333bcf7d5017c0a987e92d1
jbhagat <1@1.com>
2017-01-21 22:37:47 -0500

    added google oauth

--- app/src/main/java/com/vaultapp/pennapps/vaultapp/MainActivity.java
+++ app/src/main/java/com/vaultapp/pennapps/vaultapp/MainActivity.java
@@ -3,29 +3,66 @@ package com.vaultapp.pennapps.vaultapp;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
 import com.reimaginebanking.api.nessieandroidsdk.constants.AccountType;
 import com.reimaginebanking.api.nessieandroidsdk.models.Account;
 import com.vaultapp.pennapps.vaultapp.security.SecurityStoreSingleton;
 
 import java.security.SecureRandom;
+import java.util.Set;
+
+import static android.content.ContentValues.TAG;
 
 public class MainActivity extends AppCompatActivity {
 
     public static Context ctx = null;
     public static String appDataDir = "";
     private static SecureRandom rand = new SecureRandom();
+    private static GoogleApiClient mGoogleApiClient = null;
+    private final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.sign_in_button:
+                        if (!mGoogleApiClient.isConnected()) {
+                            mGoogleApiClient.connect();
+                        }
+                        signIn();
+                        break;
+                    // ...
+                }
+            }
+        });
         MainActivity.ctx = MainActivity.this;
         appDataDir = MainActivity.ctx.getFilesDir().getAbsolutePath();
-
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestScopes(new Scope(Scopes.PLUS_LOGIN))
+                .requestScopes(new Scope(Scopes.PLUS_ME))
+                .requestEmail().requestIdToken(getResources().getString(R.string.glclientidweb))
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, null)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
     }
 
 
@@ -64,7 +101,33 @@ public class MainActivity extends AppCompatActivity {
         return builder.toString();
     }
 
+
     protected void Login(View v) {
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    //For Google SignON
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Set<String> datas = data.getExtras().keySet();
+        String ext = data.getStringExtra("googleSignInStatus");
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        Toast.makeText(MainActivity.ctx, "before", Toast.LENGTH_SHORT).show();
+        GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+        Toast.makeText(MainActivity.ctx, "after", Toast.LENGTH_SHORT).show();
+        if (requestCode == RC_SIGN_IN) {
+            result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                Toast.makeText(MainActivity.ctx, "Successfully signed in w/ Google", Toast.LENGTH_SHORT).show();
+            } else {
+                //Toast.makeText(MainActivity.ctx, result.getStatus().getStatusCode(), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
 }
